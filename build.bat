fc2f292fbb73b333d312d8d8afdea55c8f72ef3f
Christopher <cpsuperstoreinc@gmail.com>
2022-06-18 00:31:16 -0400

    Added hidden pyttsx3 hidden import to build script

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @RD /S /Q build
 @RD /S /Q dist
 
-pyinstaller BackSeatGamerReverseProxy.py --onefile --name "ReverseProxy" -i assets/logo.ico -y --clean --noconsole
+pyinstaller BackSeatGamerReverseProxy.py --onefile --name "ReverseProxy" -i assets/logo.ico -y --clean --noconsole --hidden-import=pyttsx3.drivers
 
 cp assets dist -r
 
