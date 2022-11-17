60e2fb75698a342051a9c0645420aecd6e91764c
Christopher <cpsuperstoreinc@gmail.com>
2022-11-16 21:46:37 -0500

    Added hidden imports to build script

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @RD /S /Q build
 @RD /S /Q dist
 
-pyinstaller BackSeatGamerReverseProxy.py --onefile --name "ReverseProxy" -i assets/logo.ico -y --clean --noconsole --hidden-import=pyttsx3.drivers --hidden-import=pyttsx3.drivers.sapi5
+pyinstaller BackSeatGamerReverseProxy.py --onefile --name "ReverseProxy" -i assets/logo.ico -y --clean --noconsole --hidden-import=pyttsx3.drivers --hidden-import=pyttsx3.drivers.sapi5 --hidden-import "pynput.keyboard._win32" --hidden-import "pynput.mouse._win32"
 
 cp assets dist -r
 
